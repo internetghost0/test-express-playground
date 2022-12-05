0ef05b1d6332ea95c29d34c65496ccd7a46489a5
zer0 <internetghost0@protonmail.com>
2022-12-05 17:45:56 +0000

    add `express-http-basics` example

---
+++ examples/03-express-http-basics.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
+app.get("/about", (req, res) => {
+    res.send("About Page")
+})
+
+app.all('*', (req,res)=>{
+    res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000...")
+})
