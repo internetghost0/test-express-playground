0b54d8d0c749cdf63a1db3f7d156c769dea6e3a4
zer0 <internetghost0@protonmail.com>
2022-12-05 17:08:05 +0000

    add a new `http content-type` example

---
+++ examples/01-http-headers.js
@@ -0,0 +1,17 @@
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.end("<h1>home page</h1>")
+    } else if (req.url === '/plain') {
+        res.writeHead(200, { "content-type": "text/plain" })
+        res.end("<h1>home page</h1>")
+    } else {
+        res.writeHead(404, { "content-type": "text/html" })
+        res.write("not found")
+        res.end()
+    }
+})
+
+server.listen(5000)
