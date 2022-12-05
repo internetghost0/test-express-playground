c1d80b98d0319ca8f8e0de2c92856d1ec8b83363
zer0 <internetghost0@protonmail.com>
2022-12-05 17:18:05 +0000

    starting to create a simple webapp w/o express

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Express Tutorial')
+const http = require("http")
+const { readFileSync } = require("fs")
+
+// get all files
+const homePage = readFileSync("./index.html")
+
+const server = http.createServer((req, res) => {
+    // home page
+    if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.write(homePage)
+        res.end()
+    }
+    // about page
+    else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/html" })
+        res.write("about :)")
+        res.end("<h1>home page</h1>")
+    }
+    // 404 not found
+    else {
+        res.writeHead(404, { "content-type": "text/html" })
+        res.write("not found")
+        res.end()
+    }
+})
+
+server.listen(5000)
